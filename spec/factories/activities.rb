efd76b9d7e5391d344bfcfcf541f4a4f5376896b
rsmokeUM <rsmoke@umich.edu>
2024-11-21 17:50:46 -0500

    The factory follows the validations defined in the Activity model:
    Presence of description
    Presence of date_occurs
    Presence of cost_cents and numericality
    Association with camp_occurrence

--- spec/factories/activities.rb
+++ spec/factories/activities.rb
@@ -13,10 +13,26 @@
 #
 FactoryBot.define do
   factory :activity do
-    session { nil }
-    description { "MyString" }
-    cost_cents { 20000 }
-    date_occurs { "2019-07-23" }
+    association :camp_occurrence
+    description { Faker::Lorem.sentence }
+    cost_cents { Faker::Number.between(from: 1000, to: 100000) }
+    date_occurs { Faker::Date.between(from: 1.year.ago, to: 1.year.from_now) }
     active { false }
+
+    trait :active do
+      active { true }
+    end
+
+    # Create an activity for a specific date range
+    trait :for_camp_dates do
+      after(:build) do |activity|
+        if activity.camp_occurrence
+          activity.date_occurs = Faker::Date.between(
+            from: activity.camp_occurrence.begin_date,
+            to: activity.camp_occurrence.end_date
+          )
+        end
+      end
+    end
   end
 end
