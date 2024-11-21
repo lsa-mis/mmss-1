5ef263847aa1c2a8ec6e0720277d6511705b2222
rsmokeUM <rsmoke@umich.edu>
2024-11-21 18:58:33 -0500

    add factory

--- spec/factories/admins.rb
+++ spec/factories/admins.rb
@@ -21,6 +21,33 @@
 #
 FactoryBot.define do
   factory :admin do
-    
+    email { Faker::Internet.unique.email }
+    password { 'password123' }
+    password_confirmation { 'password123' }
+    sign_in_count { 0 }
+    current_sign_in_at { nil }
+    last_sign_in_at { nil }
+    current_sign_in_ip { nil }
+    last_sign_in_ip { nil }
+    failed_attempts { 0 }
+    unlock_token { nil }
+    locked_at { nil }
+    reset_password_token { nil }
+    reset_password_sent_at { nil }
+    remember_created_at { nil }
+
+    trait :locked do
+      failed_attempts { 3 }
+      locked_at { Time.current }
+      unlock_token { Devise.friendly_token }
+    end
+
+    trait :with_sign_in_history do
+      sign_in_count { 1 }
+      current_sign_in_at { Time.current }
+      last_sign_in_at { 1.day.ago }
+      current_sign_in_ip { '127.0.0.1' }
+      last_sign_in_ip { '127.0.0.1' }
+    end
   end
 end
